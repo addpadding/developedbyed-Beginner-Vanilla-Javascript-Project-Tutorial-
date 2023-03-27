c2bc80da6a4d3fe3cae59f8db312891f4ae12eed
addpadding <tiktokaddison20@gmail.com>
2023-03-28 01:07:14 +0300

    23.1%

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const input = document.querySelector(".input")
+const btn_plus = document.querySelector(".btn")
+const ul_list = document.querySelector(".ul_list")
+
+btn_plus.addEventListener("click", add_todo)
+
+function add_todo(e) {
+    e.preventDefault();
+
+    const div = document.createElement("div")
+    div.classList.add("div_todo")
+
+    const li = document.createElement("li")
+    li.innerText = "hey"
+    li.classList.add("li_item")
+    div.appendChild(li)
+
+    const complete_btn = document.createElement("button")
+    complete_btn.innerHTML = '<i class="fas fa-check"></i>'
+    complete_btn.classList.add("complete_btn")
+    div.appendChild(complete_btn)
+
+    const trash_btn = document.createElement("button")
+    trash_btn.innerHTML = '<i class="fas fa-trash"></i>'
+    trash_btn.classList.add("trash_btn")
+    div.appendChild(trash_btn)
+
+    ul_list.appendChild(div)
+
+
+}
