5dc14acdc8ea5dd51a66241bb1a072b0e9689070
addpadding <tiktokaddison20@gmail.com>
2023-03-28 21:32:56 +0300

    50.9%

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-const input = document.querySelector(".input")
-const btn_plus = document.querySelector(".btn_plus")
-const ul_list = document.querySelector(".ul_list")
+var input = document.querySelector(".input")
+var btn_plus = document.querySelector(".btn_plus")
+var ul_list = document.querySelector(".ul_list")
 
 btn_plus.addEventListener("click", add_todo)
 
 function add_todo(e) {
     e.preventDefault();
 
-    const div = document.createElement("div")
+    var div = document.createElement("div")
     div.classList.add("div_todo")
 
-    const li = document.createElement("li")
+    var li = document.createElement("li")
     li.innerText = "hey"
     li.classList.add("li_item")
     div.appendChild(li)
 
-    const complete_btn = document.createElement("button")
+    var complete_btn = document.createElement("button")
     complete_btn.innerHTML = '<i class="fas fa-check"></i>'
     complete_btn.classList.add("complete_btn")
     div.appendChild(complete_btn)
 
-    const trash_btn = document.createElement("button")
+    var trash_btn = document.createElement("button")
     trash_btn.innerHTML = '<i class="fas fa-trash"></i>'
     trash_btn.classList.add("trash_btn")
     div.appendChild(trash_btn)
 
     ul_list.appendChild(div)
-
-
 }
